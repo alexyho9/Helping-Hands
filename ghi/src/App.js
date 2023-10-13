1e280155b73181a99f1da228dbd85f8abc0f7822
Naniiscoding <ariannym.o@gmail.com>
2023-10-13 21:57:55 +0000

    Resolve "REACT - Meals List Page"

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -6,37 +6,38 @@ import ListEvents from "./events/ListEvents.js";
 import useToken from "@galvanize-inc/jwtdown-for-react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import EventDetails from "./events/EventDetails";
+import ListMeals from "./meals/ListMeals";
 import LoginForm from "./users/LoginForm";
 import SignupForm from "./users/SignUpForm";
 
 function App() {
   const { token, fetchWithCookie } = useToken();
   const [user, setUser] = useState({});
-   const fetchLoggedInUser = async () => {
-     if (token) {
-       const newToken = await fetchWithCookie(
-         `${process.env.REACT_APP_API_HOST}/token`
-       );
+  const fetchLoggedInUser = async () => {
+    if (token) {
+      const newToken = await fetchWithCookie(
+        `${process.env.REACT_APP_API_HOST}/token`
+      );
 
-       const account = newToken.account;
-       setUser(account);
-     }
-   };
+      const account = newToken.account;
+      setUser(account);
+    }
+  };
 
-   useEffect(() => {
-     fetchLoggedInUser();
-   }, [token]);
-return(
-<BrowserRouter>
-  <Routes>
-    <Route path="events" element={<ListEvents/>} />
-    <Route path="events/:id" element={<EventDetails/>}/>
-    <Route path="login" element={<LoginForm/>}/>
-    <Route path="signup" element={<SignupForm/>}/>
-  </Routes>
-</BrowserRouter>
-
-)
+  useEffect(() => {
+    fetchLoggedInUser();
+  }, [token]);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="events" element={<ListEvents />} />
+        <Route path="events/:id" element={<EventDetails />} />
+        <Route path="login" element={<LoginForm />} />
+        <Route path="meals" element={<ListMeals />} />
+        <Route path="signup" element={<SignupForm />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ ghi/src/meals/ListMeals.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+
+const ListMeals = () => {
+  const [meals, setMealsList] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:8000/api/meals");
+        if (response.ok) {
+          const data = await response.json();
+          setMealsList(data);
+        } else {
+          console.error("Failed to fetch meals");
+        }
+      } catch (error) {
+        console.error("An error occurred while fetching the data: ", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Meal List</h1>
+      <ul>
+        {meals.map((meal, index) => (
+          <li key={index}>
+            <h2>{meal.title}</h2>
+            <p>Date: {meal.date}</p>
+            <p>Description: {meal.description}</p>
+            <p>Capacity: {meal.capacity}</p>
+            <img src={meal.image_url} alt={meal.title} width={100} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ListMeals;
