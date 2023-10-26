8ee8aba06d42ef3dc8d84d1a6e0af8f5528a98ed
Alexander Ho <alexyho9@gmail.com>
2023-10-26 20:17:53 +0000

    text changes to home

--- ghi/src/MainPage/Main.js
+++ ghi/src/MainPage/Main.js
@@ -1,27 +1,32 @@
-import * as React from 'react';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-
+import * as React from "react";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
 
 function Main() {
-
   return (
-    <Grid
-      item
-      xs={12}
-      md={8}
-    >
+    <Grid item xs={12} md={8}>
       <Typography variant="h6" gutterBottom>
         About Us
       </Typography>
       <Divider />
-        <Typography variant="p" gutterBottom>
-          Put some info in here
-        </Typography>
+      <Typography variant="p" display="block" gutterBottom>
+        Helping Hands is a Non-Profit organization that attempts to harness the
+        potential of motivated individuals to work together and solve problems
+        in the community. Volunteers can sign up to view different volunteering
+        events coming up in their area, and also sign up to attend those events.
+        Registered volunteers are given a profile and can manage their events
+        from the webpage.
+      </Typography>
+      <Typography variant="p" display="block" gutterBottom>
+        In addition, Helping Hands has an onsite community kitchen which serves
+        food daily to the community free of charge. Guests are able to see the
+        list of upcoming meals and sign up for a spot at that meal. Non-profit
+        staff is also able to see the list of meal reservations to anticipate a
+        meal’s attendance.
+      </Typography>
     </Grid>
   );
 }
-
 
 export default Main;
--- ghi/src/MainPage/MainPage.js
+++ ghi/src/MainPage/MainPage.js
@@ -1,54 +1,49 @@
-import * as React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
-import Container from '@mui/material/Container';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Header from './Header';
-import MainFeaturedPost from './MainFeaturedPost';
-import FeaturedPost from './FeaturedPost';
-import Main from './Main';
-import Sidebar from './Sidebar';
-import Footer from './Footer';
-
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Header from "./Header";
+import MainFeaturedPost from "./MainFeaturedPost";
+import FeaturedPost from "./FeaturedPost";
+import Main from "./Main";
+import Sidebar from "./Sidebar";
+import Footer from "./Footer";
 
 const mainFeaturedPost = {
-  title: 'Basic Info',
-  description:
-    "Some made up info about helping hands in a short manner.",
-  image: 'https://source.unsplash.com/random?volunteer',
-  imageText: 'main image description',
+  title: "Basic Info",
+  description: "Some made up info about helping hands in a short manner.",
+  image: "https://source.unsplash.com/random?volunteer",
+  imageText: "main image description",
 };
 const featuredPosts = [
   {
-    title: 'Events',
+    title: "Events",
     description:
-      'This is all the events that helping hands is currently organizing and if you would like to volunteer for an event.',
-    image: 'https://source.unsplash.com/random?event',
-    imageLabel: 'Events',
-    href: "/events"
+      "Here are all the volunteering events that helping hands is currently organizing. To view and join our events, please sign up as a volunteer.",
+    image: "https://source.unsplash.com/random?event",
+    imageLabel: "Events",
+    href: "/events",
   },
   {
-    title: 'Kitchen',
+    title: "Kitchen",
     description:
-      'This is all the meals that helping hands is currently offering to those that are in need.',
-    image: 'https://source.unsplash.com/random?meal',
-    imageLabel: 'Meals',
-    href: "/meals"
+      "The Helping Hands Kitchen is open to anyone in the community who wants a meal free of charge. Lunch is served daily between the hours of 12 and 2 PM. ",
+    image: "https://source.unsplash.com/random?meal",
+    imageLabel: "Meals",
+    href: "/meals",
   },
 ];
 
-
-
 const sidebar = {
   social: [
-    { name: 'LilyGrey', icon: GitHubIcon },
-    { name: 'JackPiercy', icon: GitHubIcon },
-    { name: 'alexyho9', icon: GitHubIcon },
-    { name: 'Naniiscoding', icon: GitHubIcon },
+    { name: "LilyGrey", icon: GitHubIcon },
+    { name: "JackPiercy", icon: GitHubIcon },
+    { name: "alexyho9", icon: GitHubIcon },
+    { name: "Naniiscoding", icon: GitHubIcon },
   ],
 };
-
 
 const defaultTheme = createTheme();
 
@@ -57,7 +52,7 @@ export default function MainPage() {
     <ThemeProvider theme={defaultTheme}>
       <CssBaseline />
       <Container maxWidth="lg">
-        <Header/>
+        <Header />
         <main>
           <MainFeaturedPost post={mainFeaturedPost} />
           <Grid container spacing={4}>
@@ -66,14 +61,12 @@ export default function MainPage() {
             ))}
           </Grid>
           <Grid container spacing={5} sx={{ mt: 3 }}>
-              <Main title="About Us" />
-            <Sidebar
-              social={sidebar.social}
-            />
+            <Main title="About Us" />
+            <Sidebar social={sidebar.social} />
           </Grid>
         </main>
       </Container>
-      <Footer/>
+      <Footer />
     </ThemeProvider>
   );
 }
